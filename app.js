314f1e7d2247f10912f1cc780ff60002c43c04ad
daridev <darideveloper@gmail.com>
2023-07-10 10:54:19 -0600

    feat (app): save current_streams

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
+let current_streams = []
+
 app.use(express.json())
 
 app.get ('/', (req, res) => {
   res.send('app running')
+})
+
+app.get ('/streams/', (req, res) => {
+  res.send({streams: current_streams})
 })
 
 app.post('/', (req, res) => {
@@ -33,9 +39,19 @@ app.post('/', (req, res) => {
 
   // Loop each stream
   for (const stream of streams) {
+
+    // Save current stream
+    current_streams.push(stream.user_name)
+
+    // Start chat reader
     bot.read_chat(stream).then((res) => {
+
       // Remove current stream from live streams
       saveLog (`Stream ${stream.user_name} ended.`)
+
+      // Remove current stream from live streams
+      current_streams = current_streams.filter(item => item !== stream.user_name)
+
       return "Thread end"
     })
   }
